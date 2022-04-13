24cda70429f632a83d6645d0d807e2a87049ce81
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2022-04-13 19:49:08 +0300

    Add https to images

--- src/controllers/media.js
+++ src/controllers/media.js
@@ -4,7 +4,7 @@ function uploadImage(req, res, next) {
     if (!req.file) {
         return next([new PhotoInvalidError()]);
     } else {
-        const photoUrl = req.protocol + "://" + req.hostname + "/" + req.file.path;
+        const photoUrl = req.protocol + 's' + "://" + req.hostname + "/" + req.file.path;
         res.status(201).json({
             photoUrl: photoUrl,
         });
