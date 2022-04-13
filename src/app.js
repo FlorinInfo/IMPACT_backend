19b44c0da0084b8c739483c11df5c7665ca9b390
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2022-04-13 19:15:15 +0300

    Add Authorization header to CORS

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
     return next();
 });
